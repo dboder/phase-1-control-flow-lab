82201b897fa3ac469a8af91e8508668765e57651
Dorsey B <dorseybec@gmail.com>
2023-04-15 12:26:27 -0700

    complete

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  let return_value;
+  if (value <= 400) {
+    return_value = "This one is on me!";
+  }
+  else if (value > 400 && value <= 2000){
+    return_value = "That will be twenty bucks.";
+  }
+  else if (value > 2000 && value < 2500){
+    return_value = "I will gladly take your thirty bucks.";
+  }
+  else {
+    return_value = "No can do.";
+  }
+  return return_value;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let return_value;
+  switch (tip) {
+    case 'generous':
+      return_value = "Thank you so much.";
+      break;
+    case 'not as generous':
+      return_value = 'Thank you.';
+      break;
+
+    default:
+      return_value = "Bye."
+      break;
+  }
+  return return_value;
 }
